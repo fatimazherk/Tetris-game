ae33ed02fbeea45dbb2b075918dfc4cc15f1e52c
Fatimaa Khan <164335042+fatimazherk@users.noreply.github.com>
2024-06-21 23:37:10 -0700

    Update block.cpp

--- block.cpp
+++ block.cpp
@@ -1,38 +1,55 @@
 #include "block.h"
 
-Block::Block() {
+Block::Block()
+{
     cellSize = 30;
-    rotationSize = 0;
-    colors = GetCellColors(); // Assuming this function is defined elsewhere and returns a vector of Color
+    rotationState = 0;
+    colors = GetCellColors();
     rowOffset = 0;
     columnOffset = 0;
 }
 
-void Block::Draw() {
+void Block::Draw(int offsetX, int offsetY)
+{
     std::vector<Position> tiles = GetCellPositions();
-    for (Position item : tiles) {
-        DrawRectangle(item.column * cellSize + 1, item.row * cellSize + 1, cellSize - 1, cellSize - 1, colors[id]);
+    for (Position item : tiles)
+    {
+        DrawRectangle(item.column * cellSize + offsetX, item.row * cellSize + offsetY, cellSize - 1, cellSize - 1, colors[id]);
     }
 }
 
-void Block::Move(int rows, int columns) {
+void Block::Move(int rows, int columns)
+{
     rowOffset += rows;
     columnOffset += columns;
 }
 
-std::vector<Position> Block::GetCellPositions() {
-    std::vector<Position> tiles = cells[rotationSize];
+std::vector<Position> Block::GetCellPositions()
+{
+    std::vector<Position> tiles = cells[rotationState];
     std::vector<Position> movedTiles;
-    for (Position item : tiles) {
-        Position newPos(item.row + rowOffset, item.column + columnOffset);
+    for (Position item : tiles)
+    {
+        Position newPos = Position(item.row + rowOffset, item.column + columnOffset);
         movedTiles.push_back(newPos);
     }
     return movedTiles;
 }
 
-void Block::Rotate(){
-    rotationSize ++;
-    if(rotationSize == (int)cells.size()){
-        rotationSize=0;
+void Block::Rotate()
+{
+    rotationState++;
+    if (rotationState == (int)cells.size())
+    {
+        rotationState = 0;
+    }
+}
+
+void Block::UndoRotation()
+{
+    rotationState--;
+    if (rotationState == -1)
+    {
+        rotationState = cells.size() - 1;
     }
 }
